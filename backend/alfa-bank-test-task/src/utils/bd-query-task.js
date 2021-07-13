f01f9b7226eab10dd5d8b16134be3d40bdc5e0f5
Bulat <bulatbakirov_5130@mail.ru>
2021-07-13 11:50:57 +0300

    Edit bd-query-task

--- backend/alfa-bank-test-task/src/utils/bd-query-task.js
+++ backend/alfa-bank-test-task/src/utils/bd-query-task.js
@@ -1,9 +1,11 @@
-`CREATE VIEW Value
-AS SELECT Value as value
-FROM Table1
-WHERE Id_Client = ${clientId};
-SELECT Amount, DocDate as Date, Caption FROM Table2
-WHERE Id_Client = ${clientId}
-AND 10 < (SELECT * FROM Value)
-UNION ALL (SELECT value, '', '' FROM Value)
-ORDER BY DocDate DESC`;
+`SELECT ss.Id_Client, ss.DocDate, Amount =
+CASE
+WHEN sum < t1.Value THEN ss.Amount
+WHEN sum-Amount < t1.Value THEN t1.Value + ss.Amount - sum
+END, ss.Caption
+FROM (SELECT Id_Client, DocDate, Amount, Caption,
+SUM(Amount) OVER(PARTITION BY Id_Client ORDER BY DocDate DESC, Caption DESC) AS sum FROM Table2) AS ss
+LEFT JOIN Table1 as t1 ON t1.Id_Client = ss.Id_Client
+WHERE ss.sum < t1.Value
+OR ss.sum-ss.Amount < t1.Value
+ORDER BY ss.Id_Client, ss.DocDate DESC`;
